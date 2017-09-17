3baf7ea8efb055e193d00363cdcb07a45a36c1b0
Vinh96 <vinh.that@gmail.com>
2017-09-17 18:38:17 +0700

    Init project

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebThueXe.Startup))]
+namespace WebThueXe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
